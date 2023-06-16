d13e08c9fe39018336b0f8cb748afa9606c4e84e
sprpvt310 <sprpvt310@gmail.com>
2023-06-16 22:53:36 +0900

    a

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,10 @@ module.exports = defineConfig({
   devServer: {
     port: 8080,
     client: {
-      webSocketURL: 'ws://0.0.0.0:8080/ws',
+      webSocketURL: "ws://0.0.0.0:8080/ws",
     },
-    allowedHosts: 'all',
+    allowedHosts: "all",
   },
+  outputDir: "docs",
+  publicPath: "./",
 });
